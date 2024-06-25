65b12b230a188ba772f6c66c733cf586fe005581
jananga <kavinduwijewardhana1999@gmail.com>
2024-06-25 19:21:49 +0530

    add header

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li onClick={()=> setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-        <li onClick={()=> setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
-        <li onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-app</li>
-        <li onClick={()=> setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact us</li>
+        <li onClick={()=> setMenu("home")} className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=> setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=> setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
